pbatard/ntfs-3g

commit 79c12b1899942f9023456c002a24c4bb4ead14eb

Fix Coverity warnings

libntfs-3g/index.c
@@ -1820,7 +1820,7 @@ int ntfs_index_rm(ntfs_index_context *icx)
 
 	ntfs_log_trace("Entering\n");
 	
-	if (!icx || (!icx->ib && !icx->ir) || ntfs_ie_end(icx->entry)) {
+	if (!icx || (icx->is_in_root && !icx->ib) || (!icx->is_in_root && !icx->ir) || ntfs_ie_end(icx->entry)) {
 		ntfs_log_error("Invalid arguments.\n");
 		errno = EINVAL;
 		goto err_out;

include/ntfs-3g/types.h
@@ -115,6 +115,11 @@ typedef enum {
 #endif /* defined _WINDEF_H */
 
 /**
+ * Can be used to silence warnings about not checking a returned value.
+ */
+#define IGNORE_RETVAL(expr)  do { (void)(expr); } while(0)
+
+/**
  * enum IGNORE_CASE_BOOL -
  */
 typedef enum {
@@ -129,4 +134,3 @@ typedef enum {
 #define STATUS_NOT_FOUND			(-4)
 
 #endif /* defined _NTFS_TYPES_H */
-

libntfs-3g/acls.c
@@ -4407,6 +4407,7 @@ struct MAPPING *ntfs_do_user_mapping(struct MAPLIST *firstitem)
 			    && !ntfs_valid_pattern(sid)) {
 				ntfs_log_error("Bad implicit SID pattern %s\n",
 					item->sidstr);
+				ntfs_free(sid);
 				sid = (SID*)NULL;
 				}
 			if (sid) {
@@ -4423,7 +4424,8 @@ struct MAPPING *ntfs_do_user_mapping(struct MAPLIST *firstitem)
 					else
 						firstmapping = mapping;
 					lastmapping = mapping;
-				}
+				} else
+					ntfs_free(sid);
 			}
 		}
 	}
@@ -4491,6 +4493,7 @@ struct MAPPING *ntfs_do_group_mapping(struct MAPLIST *firstitem)
 				if (sid && !item->uidstr[0] && !item->gidstr[0]
 				    && !ntfs_valid_pattern(sid)) {
 					/* error already logged */
+					ntfs_free(sid);
 					sid = (SID*)NULL;
 					}
 				if (sid) {

libntfs-3g/attrib.c
@@ -1362,6 +1362,8 @@ static int ntfs_attr_fill_hole(ntfs_attr *na, s64 count, s64 *ofs,
 		na->compressed_size += need << vol->cluster_size_bits;
 	
 	*rl = ntfs_runlists_merge(na->rl, rlc);
+	if (*rl)
+		rlc = NULL;
 	NAttrSetRunlistDirty(na);
 		/*
 		 * For a compressed attribute, we must be sure there are two
@@ -2523,7 +2525,7 @@ retry:
 	if (ctx)
 		ntfs_attr_put_search_ctx(ctx);
 	/* Update mapping pairs if needed. */
-	if (NAttrFullyMapped(na))
+	if (na && NAttrFullyMapped(na))
 		if (ntfs_attr_update_mapping_pairs(na, update_from)) {
 			/*
 			 * FIXME: trying to recover by goto rl_err_out; 
@@ -2533,7 +2535,8 @@ retry:
 			goto out;
 	}
 out:	
-	NAttrClearComprClosing(na);
+	if (na)
+		NAttrClearComprClosing(na);
 	ntfs_log_leave("\n");
 	return (!ok);
 rl_err_out:
@@ -2548,7 +2551,7 @@ err_out:
 	if (ctx)
 		ntfs_attr_put_search_ctx(ctx);
 	/* Update mapping pairs if needed. */
-	if (NAttrFullyMapped(na))
+	if (na && NAttrFullyMapped(na))
 		ntfs_attr_update_mapping_pairs(na, 0);
 	errno = eo;
 errno_set:

libntfs-3g/compress.c
@@ -720,12 +720,17 @@ s64 ntfs_compressed_attr_pread(ntfs_attr *na, s64 pos, s64 count, void *b)
 	FILE_ATTR_FLAGS compression;
 	unsigned int nr_cbs, cb_clusters;
 
+	if (!na || !na->ni) {
+		errno = EINVAL;
+		return -1;
+	}
+
 	ntfs_log_trace("Entering for inode 0x%llx, attr 0x%x, pos 0x%llx, count 0x%llx.\n",
 			(unsigned long long)na->ni->mft_no, le32_to_cpu(na->type),
 			(long long)pos, (long long)count);
 	data_flags = na->data_flags;
 	compression = na->ni->flags & FILE_ATTR_COMPRESSED;
-	if (!na || !na->ni || !na->ni->vol || !b
+	if (!na->ni->vol || !b
 			|| ((data_flags & ATTR_COMPRESSION_MASK)
 				!= ATTR_IS_COMPRESSED)
 			|| pos < 0 || count < 0) {
@@ -1932,8 +1937,8 @@ int ntfs_compressed_close(ntfs_attr *na, runlist_element *wrl, s64 offs,
 				}
 			} else
 				done = TRUE;
-			free(inbuf);
 		}
+		ntfs_free(inbuf);
 	}
 	if (done && !valid_compressed_run(na,wrl,TRUE,"end compressed close"))
 		done = FALSE;

libntfs-3g/dir.c
@@ -252,7 +252,7 @@ u64 ntfs_inode_lookup_by_name(ntfs_inode *dir_ni,
 	VCN vcn;
 	u64 mref = 0;
 	s64 br;
-	ntfs_volume *vol = dir_ni->vol;
+	ntfs_volume *vol;
 	ntfs_attr_search_ctx *ctx;
 	INDEX_ROOT *ir;
 	INDEX_ENTRY *ie;
@@ -270,6 +270,7 @@ u64 ntfs_inode_lookup_by_name(ntfs_inode *dir_ni,
 		errno = EINVAL;
 		return -1;
 	}
+	vol = dir_ni->vol;
 
 	ctx = ntfs_attr_get_search_ctx(dir_ni, NULL);
 	if (!ctx)
@@ -601,10 +602,11 @@ u64 ntfs_inode_lookup_by_mbsname(ntfs_inode *dir_ni, const char *name)
 			{
 				/* Generate unicode name. */
 			uname_len = ntfs_mbstoucs(cached_name, &uname);
-			if (uname_len >= 0)
+			if (uname_len >= 0) {
 				inum = ntfs_inode_lookup_by_name(dir_ni,
 						uname, uname_len);
-			else
+				free(uname);
+			} else
 				inum = (s64)-1;
 		}
 		if (cached_name)
@@ -2719,6 +2721,8 @@ int ntfs_set_ntfs_dos_name(ntfs_inode *ni, ntfs_inode *dir_ni,
 	    || ntfs_forbidden_names(ni->vol,shortname,shortlen,TRUE)) {
 		ntfs_inode_close_in_dir(ni,dir_ni);
 		ntfs_inode_close(dir_ni);
+		if (shortlen >= 0)
+			ntfs_free(shortname);
 		res = -errno;
 		return res;
 	}

libntfs-3g/inode.c
@@ -346,6 +346,8 @@ int ntfs_inode_real_close(ntfs_inode *ni)
 		base_ni = ni->base_ni;
 		for (i = 0; i < base_ni->nr_extents; ++i) {
 			tmp_nis = base_ni->extent_nis;
+			if (tmp_nis == NULL)
+				break;
 			if (tmp_nis[i] != ni)
 				continue;
 			/* Found it. Disconnect. */

libntfs-3g/lcnalloc.c
@@ -580,6 +580,11 @@ int ntfs_cluster_free_from_rl(ntfs_volume *vol, runlist *rl)
 
 	ntfs_log_trace("Entering.\n");
 
+	if (!vol || !rl) {
+		ntfs_log_trace("Invalid arguments!\n");
+		errno = EINVAL;
+		return -1;
+	}
 	for (; rl->length; rl++) {
 
 		ntfs_log_trace("Dealloc lcn 0x%llx, len 0x%llx.\n",

commit 5e941be22542f9c90621c057c6c5e423732a6a96

Fix EINVAL when deleting files
* 79c12b1899942f9023456c002a24c4bb4ead14eb introduced a regression/issue with file deletion, where attempting to delete a file
  via the standard UEFI API may EFI_INVALID_PARAMETER (internal errno = 22, EINVAL), and the file was not deleted.
* This is due to the inversion of the icx->ib and icx->ir variables in the additional conditions that were added
  to index.c to fix Coverity warnings, so we fix that.
* Closes #13.
* Closes #9.

libntfs-3g/index.c
@@ -1922,7 +1922,7 @@ int ntfs_index_rm(ntfs_index_context *icx)
 
 	ntfs_log_trace("Entering\n");
 	
-	if (!icx || (icx->is_in_root && !icx->ib) || (!icx->is_in_root && !icx->ir) || ntfs_ie_end(icx->entry)) {
+	if (!icx || (icx->is_in_root && !icx->ir) || (!icx->is_in_root && !icx->ib) || ntfs_ie_end(icx->entry)) {
 		ntfs_log_error("Invalid arguments.\n");
 		errno = EINVAL;
 		goto err_out;
5e941be22542f9c90621c057c6c5e423732a6a96
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2026-06-06 15:52:26 +0100

    Fix EINVAL when deleting files
    * 79c12b1899942f9023456c002a24c4bb4ead14eb introduced a regression/issue with file deletion, where attempting to delete a file
      via the standard UEFI API may EFI_INVALID_PARAMETER (internal errno = 22, EINVAL), and the file was not deleted.
    * This is due to the inversion of the icx->ib and icx->ir variables in the additional conditions that were added
      to index.c to fix Coverity warnings, so we fix that.
    * Closes #13.
    * Closes #9.

--- libntfs-3g/index.c
+++ libntfs-3g/index.c
@@ -1922,7 +1922,7 @@ int ntfs_index_rm(ntfs_index_context *icx)
 
 	ntfs_log_trace("Entering\n");
 	
-	if (!icx || (icx->is_in_root && !icx->ib) || (!icx->is_in_root && !icx->ir) || ntfs_ie_end(icx->entry)) {
+	if (!icx || (icx->is_in_root && !icx->ir) || (!icx->is_in_root && !icx->ib) || ntfs_ie_end(icx->entry)) {
 		ntfs_log_error("Invalid arguments.\n");
 		errno = EINVAL;
 		goto err_out;
